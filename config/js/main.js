de07f072376c11829a2e262d88abf943e3d26e85
Gary Albert <galbert@hasbro.com>
2015-09-22 15:55:44 -0400

    Update main.js

--- config/js/main.js
+++ config/js/main.js
@@ -16,25 +16,30 @@ function submitHandler() {
 
 function loadOptions() {
   var $backgroundColorPicker = $('#backgroundColorPicker');
+  var $foregroundColorPicker = $('#foregroundColorPicker');
   var $timeFormatCheckbox = $('#timeFormatCheckbox');
 
   if (localStorage.backgroundColor) {
     $backgroundColorPicker[0].value = localStorage.backgroundColor;
-    $timeFormatCheckbox[0].checked = localStorage.twentyFourHourFormat === 'true';
+    $foregroundColorPicker[0].value = localStorage.foregroundColor;
+    $fuzzyFormatCheckbox[0].checked = localStorage.fuzzyTextFormat === 'true';
   }
 }
 
 function getAndStoreConfigData() {
   var $backgroundColorPicker = $('#backgroundColorPicker');
-  var $timeFormatCheckbox = $('#timeFormatCheckbox');
+  var $foregroundColorPicker = $('#foregroundColorPicker');
+  var $fuzzyFormatCheckbox = $('#fuzzyFormatCheckbox');
 
   var options = {
     backgroundColor: $backgroundColorPicker.val(),
-    twentyFourHourFormat: $timeFormatCheckbox[0].checked
+    foregroundColor: $foregroundColorPicker.val(),
+    fuzzyTimeFormat: $fuzzyFormatCheckbox[0].checked
   };
 
   localStorage.backgroundColor = options.backgroundColor;
-  localStorage.twentyFourHourFormat = options.twentyFourHourFormat;
+  localStorage.foregroundColor = options.foregroundColor;
+  localStorage.fuzzyTimeFormat = options.fuzzyTimeFormat;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
